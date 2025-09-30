c6172c3d4390e1e09bd515c73963532d9243c3ae
yuliaslatvitskaya <yuli.slat@yandex.ru>
2025-09-30 14:52:41 +0200

    feat: add tariff block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ import TariffCard from "@/app/components/TariffCard";
 import "./page.css"
 
 export default function Page() {
-    const { data: tariffs, error, isLoading } = useGetItemsQuery()
+    const {data: tariffs, error, isLoading} = useGetItemsQuery()
     const [isDiscountActive, setDiscountActive] = React.useState(true)
     const [selectedId, setSelectedId] = React.useState<string | null>(null)
     const [agreeChecked, setAgreeChecked] = React.useState(false)
@@ -56,50 +56,59 @@ export default function Page() {
 
     return (
         <div className="min-h-screen bg-[#0f1720] text-white">
-            <HeaderTimer startSeconds={959} onExpire={onExpire} />
+            <HeaderTimer startSeconds={959} onExpire={onExpire}/>
 
-            <main className="max-w-6xl mx-auto px-4 pt-[120px] pb-8  rounded-b-[60px]">
-                <h1 className="text-3xl font-semibold mb-6">Выбери тариф</h1>
-
-                <div className="grid gap-6 grid-cols-1 md:grid-cols-3">
-                    {tariffs?.map((t, idx) => (
-                        <TariffCard
-                            key={`${t.id}-${idx}`}
-                            tariff={t}
-                            isDiscountActive={isDiscountActive}
-                            selected={selectedId === t.id}
-                            onSelect={() => setSelectedId(t.id)}
-                            onBuyClick={handleBuy}
-                            showBig={t.is_best}
-                        />
-                    ))}
+            <main className="container">
+                <div className="text-and-tariff">
+                    <div className="text">
+                        Выбери подходящий для себя <span className="text-tariff">тариф</span>
+                    </div>
                 </div>
 
-                <div className="mt-8 flex items-center gap-4">
-                    <label className={`flex items-center gap-2 ${buyError ? 'text-red-400' : ''}`}>
-                        <input
-                            type="checkbox"
-                            checked={agreeChecked}
-                            onChange={(e) => { setAgreeChecked(e.target.checked); setBuyError(null) }}
-                            className={`w-4 h-4 rounded ${buyError ? 'ring-2 ring-red-400' : ''}`}
-                        />
-                        <span className="text-sm">Согласен с условиями</span>
-                    </label>
 
-                    <button
-                        onClick={() => selectedId && handleBuy(selectedId)}
-                        className="ml-auto px-6 py-2 rounded bg-yellow-500 text-black font-semibold hover:opacity-90"
-                    >
-                        Купить выбранный
-                    </button>
-                    {buyError && <div className="text-red-400 text-sm ml-4">{buyError}</div>}
-                </div>
+            <div className="grid">
+                {/*<div className="grid gap-6 grid-cols-1 md:grid-cols-3">*/}
+                {tariffs?.map((t, idx) => (
+                    <TariffCard
+                        key={`${t.id}-${idx}`}
+                        tariff={t}
+                        isDiscountActive={isDiscountActive}
+                        selected={selectedId === t.id}
+                        onSelect={() => setSelectedId(t.id)}
+                        onBuyClick={handleBuy}
+                        showBig={t.is_best}
+                    />
+                ))}
+            </div>
 
-                <div className="mt-8 text-sm text-gray-300">
-                    Примечание: при окончании таймера скидочные цены исчезнут и останутся полные цены.
-                </div>
-            </main>
-        </div>
-    )
+            <div className="mt-8 flex items-center gap-4">
+                <label className={`flex items-center gap-2 ${buyError ? 'text-red-400' : ''}`}>
+                    <input
+                        type="checkbox"
+                        checked={agreeChecked}
+                        onChange={(e) => {
+                            setAgreeChecked(e.target.checked);
+                            setBuyError(null)
+                        }}
+                        className={`w-4 h-4 rounded ${buyError ? 'ring-2 ring-red-400' : ''}`}
+                    />
+                    <span className="text-sm">Согласен с условиями</span>
+                </label>
+
+                <button
+                    onClick={() => selectedId && handleBuy(selectedId)}
+                    className="ml-auto px-6 py-2 rounded bg-yellow-500 text-black font-semibold hover:opacity-90"
+                >
+                    Купить выбранный
+                </button>
+                {buyError && <div className="text-red-400 text-sm ml-4">{buyError}</div>}
+            </div>
+
+            <div className="mt-8 text-sm text-gray-300">
+                Примечание: при окончании таймера скидочные цены исчезнут и останутся полные цены.
+            </div>
+        </main>
+</div>
+)
 }
 
