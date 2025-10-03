6c735c8e44452aa25daf1fd6d885e5ecf0d2f4f1
yuliaslatvitskaya <yuli.slat@yandex.ru>
2025-10-03 16:50:49 +0200

    feat: add image and fix prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import {useGetItemsQuery} from "services/api";
 import React from 'react'
 import HeaderTimer from "@/app/components/HeaderTimer";
 import TariffCard from "@/app/components/TariffCard";
+import Image from "next/image";
 import "./page.css"
 
 export default function Page() {
@@ -41,66 +42,71 @@ export default function Page() {
             <div className="main-div">
                 <div
                     className="
-      flex justify-start font-bold
-      pt-[20px] px-[16px]          /* мобильные отступы */
-      text-[22px]                  /* 320 */
-      min-[375px]:text-[24px]      /* 375 */
-      min-[440px]:text-[40px] min-[440px]:pt-[50px]
-      min-[440px]:justify-center   /* десктоп центр */
-    "
+                        flex justify-start font-bold
+                        pt-[20px] px-[16px]
+                        text-[22px]
+                        min-[375px]:text-[24px]
+                        min-[440px]:text-[40px]
+                        min-[440px]:pt-[50px]
+                        min-[440px]:justify-center
+                        "
                 >
                     <div className="m-0">
-                        <span className="whitespace-nowrap block min-[440px]:inline">
-        Выбери подходящий
-      </span>{" "}
-                        <span className="block min-[440px]:inline">
-        для себя <span className="text-[var(--color-yellow)]">тариф</span>
-      </span>
+                        <span className="whitespace-nowrap block min-[440px]:inline">Выбери подходящий</span>{" "}
+                        <span className="block min-[440px]:inline">для себя <span
+                            className="text-[var(--color-yellow)]">тариф</span>
+                        </span>
                     </div>
                 </div>
 
-            <div className="main-conter">
-                {tariffs?.map((t, idx) => (
-                    <TariffCard
-                        key={`${t.id}-${idx}`}
-                        tariff={t}
-                        isDiscountActive={isDiscountActive}
-                        selected={selectedId === t.id}
-                        onSelect={() => setSelectedId(t.id)}
-                        onBuyClick={handleBuy}
-                        showBig={t.is_best}
-                    />
-                ))}
-            </div>
+                <div className={"container-image"}>
+                    <div className={"image"}></div>
+
+                    <div className="main-conter">
+                        {tariffs?.map((t, idx) => (
+                            <TariffCard
+                                key={`${t.id}-${idx}`}
+                                tariff={t}
+                                isDiscountActive={isDiscountActive}
+                                selected={selectedId === t.id}
+                                onSelect={() => setSelectedId(t.id)}
+                                onBuyClick={handleBuy}
+                                showBig={t.is_best}
+                            />
+                        ))}
+
 
                     <div className="desctiption">
                         Примечание: при окончании таймера скидочные цены исчезнут и останутся полные цены.
                     </div>
 
                     <div className="text-soglq">
-                    <label className={`flex items-center gap-2 ${buyError ? 'text-red-400' : ''}`}>
-                        <input
-                            type="checkbox"
-                            checked={agreeChecked}
-                            onChange={(e) => {
-                                setAgreeChecked(e.target.checked);
-                                setBuyError(null)
-                            }}
-                            className={`w-4 h-4 rounded ${buyError ? 'ring-2 ring-red-400' : ''}`}
-                        />
-                        <span className="text-sogl">Я согласен с офертой рекуррентных платежей и Политикой конфиденциальности </span>
-                    </label>
+                        <label className={`flex items-center gap-2 ${buyError ? 'text-red-400' : ''}`}>
+                            <input
+                                type="checkbox"
+                                checked={agreeChecked}
+                                onChange={(e) => {
+                                    setAgreeChecked(e.target.checked);
+                                    setBuyError(null)
+                                }}
+                                className={`w-4 h-4 rounded ${buyError ? 'ring-2 ring-red-400' : ''}`}
+                            />
+                            <span className="text-sogl">Я согласен с офертой рекуррентных платежей и Политикой конфиденциальности </span>
+                        </label>
                     </div>
+
                     <div className="btn-div">
-                    <button
-                        onClick={() => selectedId && handleBuy(selectedId)}
-                        className="btn"
-                    >
-                        Купить
-                    </button>
+                        <button
+                            onClick={() => selectedId && handleBuy(selectedId)}
+                            className="btn"
+                        >
+                            Купить
+                        </button>
                     </div>
                     {buyError && <div className="text-red-400 text-sm ml-4">{buyError}</div>}
+                    </div>
                 </div>
+
                 <div className="text-div">
                     Нажимая кнопку «Купить», Пользователь соглашается на разовое списание денежных средств для получения
                     пожизненного доступа к приложению. Пользователь соглашается, что данные кредитной/дебетовой карты
@@ -112,14 +118,18 @@ export default function Page() {
                     <div className="green-div">
                         гарантия возврата 30 дней
                     </div>
+
                     <div className="gray-div">
-                    Мы уверены, что наш план сработает для тебя и ты увидишь видимые результаты уже через 4 недели! Мы
-                    даже готовы полностью вернуть твои деньги в течение 30 дней с момента покупки, если ты не получишь
-                    видимых результатов.
-                </div>
+                        Мы уверены, что наш план сработает для тебя и ты увидишь видимые результаты уже через 4 недели!
+                        Мы
+                        даже готовы полностью вернуть твои деньги в течение 30 дней с момента покупки, если ты не
+                        получишь
+                        видимых результатов.
+                    </div>
                 </div>
 
             </div>
+        </div>
     )
 }
 
